79882194028ffc93b933ce04e6f5acc31254007a
wojciech <wojtekgg93@gmail.com>
2022-12-14 09:53:37 +0100

    added validation to participations of tournament

--- model/Participation.js
+++ model/Participation.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const Tournament = require('./Tournament')
+const Player = require('./Player')
+const { NotFoundError } = require('../errors')
 
 const ParticipationSchema = mongoose.Schema({
     player_id : {
@@ -11,6 +14,16 @@ const ParticipationSchema = mongoose.Schema({
         required: [true, "Id turnieju jest wymagane"],
         ref: 'Tournament'
     }
+})
+
+ParticipationSchema.pre('save', async function(){
+    const tournament = await Tournament.findById(this.tournament_id)
+    if(!tournament)
+        throw new NotFoundError(`Nie znaleziono turnieju o id: ${this.tournament_id}`)
+
+    const player = await Player.findById(this.player_id)
+    if(!player)
+        throw new NotFoundError(`Nie znaleziono gracza o id: ${this.player_id}`)
 })
 
 module.exports = mongoose.model('Participation', ParticipationSchema)
